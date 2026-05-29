c781ee43ea0a7f92c7f6814f3c7dd9a9b06d1df2
KodavatiAnisha <anishakodavati7@gmail.com>
2026-05-29 08:15:20 +0530

    Initial Commit

---
+++ JavaPractice/src/Token/Token.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Token {
+
+    public static void main(String[] args) {
+
+        // Characters allowed in token
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        // Object of Random class
+        Random random = new Random();
+
+        // Token length
+        int length = 10;
+
+        // Empty string to store token
+        String token = "";
+
+        // Loop to generate token
+        for (int i = 0; i < length; i++) {
+
+            // Generate random index
+            int index = random.nextInt(characters.length());
+
+            // Add random character to token
+            token += characters.charAt(index);
+        }
+
+        // Print token
+        System.out.println("Generated Token: " + token);
+    }
+}
